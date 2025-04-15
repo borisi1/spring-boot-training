7008d6ee1f5abb5530984e1068b34cb3c45ca346
borisi <borisi@borisi.com>
2025-04-15 12:43:13 -0400

    Initializing spring-boot-training repo

---
+++ backend/src/test/java/dev/usdev/spring/boot/training/SpringBootTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.usdev.spring.boot.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
